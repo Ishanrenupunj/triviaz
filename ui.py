1b7939912861aa5af33efcdafc713f2d9af51b3e
Ishanrenupunj <167489987+Ishanrenupunj@users.noreply.github.com>
2025-04-06 12:35:39 +0530

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,10 @@
+
+
+THEME_COLOR = "#375362"
+from tkinter import *
+class quizinterface:
+    def __init__(self):
+      self.window=Tk()
+      self.window.title("quizler")
+      self.window.config()
+      self.window.mainloop()
